fd7c39f0e68e253a3d66264c44cb29b254adb7a7
Dmitrii Dudkin <ddudkin@findmykids.org>
2025-03-21 14:58:57 +0100

    add timer

--- SmokeTracker/Features/Dashboard/ViewModel/DashboardViewModel.swift
+++ SmokeTracker/Features/Dashboard/ViewModel/DashboardViewModel.swift
@@ -16,14 +16,17 @@ final class DashboardViewModel: ObservableObject {
     @Published var models: [DashboardModel] = []
     
     private let storageService = StorageService.shared
+    private var timer: Timer?
     
     init () {
         update()
+        runTimer()
     }
     
     func trackSession() {
         storageService.trackSession()
         update()
+        runTimer()
     }
 }
 
@@ -49,21 +52,41 @@ private extension DashboardViewModel {
         case .numberOfSessions:
             return DashboardModel(
                 title: "Today you\nsmoked",
-                value: "\(sessions.count)",
+                value: "\(sessions.count) times",
                 backgroundColor: .dashboardYellow
             )
+            
         case .timeSinceLastSession:
             let timeSinceLast = sessions.last?.timestamp ?? Date()
             let components = Calendar.current.dateComponents([.hour, .minute], from: timeSinceLast, to: Date())
             
             let hours = components.hour ?? 0
             let minutes = components.minute ?? 0
+            
+            let dateString: String
+            
+            if hours == 0, minutes == 0 {
+                dateString = "Now"
+            } else if hours == 0 {
+                dateString = "\(minutes) minutes ago"
+            } else {
+                dateString = "\(hours) hours \(minutes) minutes ago"
+            }
                         
             return DashboardModel(
                 title: "Last time you\nsmoked",
-                value: "\(hours) hours \(minutes) minutes",
+                value: dateString,
                 backgroundColor: .dashboardYellow
             )
         }
+    }
+    
+    func runTimer() {
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true , block: { [weak self] _ in
+            print("timer")
+            self?.update()
+        })
     }
 }
